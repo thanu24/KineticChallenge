810168d7f8d8dd28c9c24ae5bde26f21165b360b
Thanu Chandrarajah <tchandrarajah@zynga.com>
2015-07-01 09:19:12 -0700

    Initial Commit

---
+++ KineticChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KineticChallenge
+//
+//  Created by Thanu Chandrarajah on 7/1/15.
+//  Copyright (c) 2015 Thanu Chandrarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KineticChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KineticChallenge
+//
+//  Created by Thanu Chandrarajah on 7/1/15.
+//  Copyright (c) 2015 Thanu Chandrarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
